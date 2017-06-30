12f5cda4a4b8a2246881a6e56b8fd83646ac70aa
JUNLEE <leejun6694@gmail.com>
2017-06-30 14:45:37 +0900

    done chapter1

---
+++ week1/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by JUN LEE on 2017. 6. 30..
+//  Copyright © 2017년 LEEJUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cognac made?", "What is 7 + 7?", "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
