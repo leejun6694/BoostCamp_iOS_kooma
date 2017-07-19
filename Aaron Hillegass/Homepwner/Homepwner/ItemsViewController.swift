7cdf89e486dcb69716afd9b8660a6cd6d91cbd94
JUNLEE <leejun6694@gmail.com>
2017-07-19 13:23:45 +0900

    update directory

--- Aaron Hillegass/Homepwner/Homepwner/ItemsViewController.swift
+++ Aaron Hillegass/Homepwner/Homepwner/ItemsViewController.swift
@@ -93,8 +93,6 @@ class ItemViewController: UITableViewController {
                 cell.valueLabel.textColor = .red
             }
         }
-            
-            /* 이 방법도 좋은 방법입니다만, 하단에 고정적인 내용을 보여주려면 더 좋은 방법은 없을까요? (꼭 row를 추가해야 한다고 생각할 필요는 없습니다) */
         // 마지막 cell
         else {
 //            cell.textLabel?.text = "No more items!"
@@ -127,17 +125,12 @@ class ItemViewController: UITableViewController {
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
             ac.addAction(cancelAction)
             
-            /*
-             https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/AutomaticReferenceCounting.html
-             문서의 Resolving Strong Reference Cycles for Closures 챕터 참고
-             */
-            let deleteAction = UIAlertAction(title: "Remove", style: .destructive, handler: {
-                [unowned self, tableView] (action) -> Void in
+            let deleteAction = UIAlertAction(title: "Remove", style: .destructive, handler: { (action) -> Void in
                 // 저장소에서 그 항목을 제거한다
                 self.itemStore.removeItem(item: item)
                 
                 // 또한 애니메이션과 함께 테이블 뷰에서 그 행을 제거한다
-                tableView.deleteRows(at: [indexPath], with: .automatic)
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
             })
             ac.addAction(deleteAction)
             
@@ -147,7 +140,12 @@ class ItemViewController: UITableViewController {
     
     // 특정 cell 재정렬 막기
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        return indexPath.row < itemStore.allItems.count
+        if indexPath.row < itemStore.allItems.count {
+            return true
+        }
+        else {
+            return false
+        }
     }
     
     // 특정 cell delete 막기
